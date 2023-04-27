89fe4c4f12ac7277372bbf0f6be6a23e1f5cf79f
Colby <colbykade626@gmail.com>
2023-04-27 18:56:07 -0500

    Fixed the theme switch

--- lib/SettingsMenu.dart
+++ lib/SettingsMenu.dart
@@ -28,17 +28,35 @@ class SettingsMenu extends StatelessWidget{
             alignment: Alignment.center,
             child: Text("Theme Mode"),
           ),
-          Switch(
-            value: true,
-            activeColor: Colors.deepPurpleAccent,
-            onChanged: (bool value) {
-              currentTheme.switchThemes();
-              value: !value;
-            }
-          )
-          //ThemeModeSwitch(),
+          SwitchExample()
         ],
       ),
+    );
+  }
+}
+
+class SwitchExample extends StatefulWidget {
+  const SwitchExample({super.key});
+
+  @override
+  State<SwitchExample> createState() => _SwitchExampleState();
+}
+
+class _SwitchExampleState extends State<SwitchExample>
+{
+  bool light = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Switch(
+      value: light,
+      activeColor: Colors.deepPurpleAccent,
+      onChanged: (bool value) {
+        currentTheme.switchThemes();
+        setState(() {
+          light = value;
+        });
+      },
     );
   }
 }
